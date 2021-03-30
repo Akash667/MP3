372d159bed42646ade5d465bb8514d783103c55b
Akash R <me@akashirl.live>
2021-03-30 10:14:13 +0530

    p1

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,19 @@ function App() {
   const [valueHook, setValueHook] = useState(20);
 
   useEffect(() => {
+    function drawImageScaled(img:HTMLImageElement, ctx: CanvasRenderingContext2D) {
+      var canvas = ctx.canvas ;
+      var hRatio = canvas.width  / img.width    ;
+      var vRatio =  canvas.height / img.height  ;
+      var ratio  = Math.min ( hRatio, vRatio );
+      var centerShift_x = ( canvas.width - img.width*ratio ) / 2;
+      var centerShift_y = ( canvas.height - img.height*ratio ) / 2;  
+      ctx.clearRect(0,0,canvas.width, canvas.height);
+      ctx.drawImage(img, 0,0, img.width, img.height,
+                         centerShift_x,centerShift_y,img.width*ratio, img.height*ratio);  
+   
+      console.log(img)
+   }
     const canvas = document.getElementById("canvas") as HTMLCanvasElement;
     const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
@@ -53,37 +66,37 @@ function App() {
           img.src = reader.result as string;
 
           img.onload = function () {
-            let height = canvas!.parentElement!.clientHeight;
-            let width = canvas!.parentElement!.clientWidth;
+            // let height = canvas!.parentElement!.clientHeight;
+            // let width = canvas!.parentElement!.clientWidth;
 
-            let ratio = img.width / img.height;
+            // let ratio = img.width / img.height;
 
-            if (img.width > img.height) {
-              if (img.width > canvas!.parentElement!.clientWidth) {
-                width = canvas!.parentElement!.clientWidth - 10;
-                height = width / ratio;
-              }
-            } else if (img.height > canvas!.parentElement!.clientHeight) {
-              height = canvas!.parentElement!.clientHeight - 10;
-              width = height * ratio;
-            }
+            // if (img.width > img.height) {
+            //   if (img.width > canvas!.parentElement!.clientWidth) {
+            //     width = canvas!.parentElement!.clientWidth - 10;
+            //     height = width / ratio;
+            //   }
+            // } else if (img.height > canvas!.parentElement!.clientHeight) {
+            //   height = canvas!.parentElement!.clientHeight - 10;
+            //   width = height * ratio;
+            // }
 
-            canvas.width = canvas!.parentElement!.clientWidth;
+            canvas.width = canvas!.parentElement!.clientWidth*0.95;
 
-            canvas.height = canvas!.parentElement!.clientHeight;
+            canvas.height = canvas!.parentElement!.clientHeight*0.95;
 
-            ctx.drawImage(
-              img,
-              0,
-              0,
-              img.width,
-              img.height,
-              0,
-              0,
-              width,
-              height
-            );
-
+            // ctx.drawImage(
+            //   img,
+            //   0,
+            //   0,
+            //   img.width,
+            //   img.height,
+            //   0,
+            //   0,
+            //   width,
+            //   height
+            // );
+              drawImageScaled(img,ctx);
             canvas.removeAttribute("data-caman-id");
           };
         },
@@ -91,17 +104,17 @@ function App() {
       );
     });
 
-    window.addEventListener("resize", () => {
-      if (canvas.width < menuDiv.offsetWidth) {
-        canvas.width = menuDiv.offsetWidth;
-      }
+    // window.addEventListener("resize", () => {
+    //   if (canvas.width < menuDiv.offsetWidth) {
+    //     canvas.width = menuDiv.offsetWidth;
+    //   }
 
-      if (canvas.height < menuDiv.offsetHeight) {
-        canvas.height = menuDiv.offsetHeight;
-      }
+    //   if (canvas.height < menuDiv.offsetHeight) {
+    //     canvas.height = menuDiv.offsetHeight;
+    //   }
 
-      ctx.drawImage(img,0,0,img.width,img.height,0,0, menuDiv.offsetWidth, menuDiv.offsetHeight)
-    });
+    //   ctx.drawImage(img,0,0,img.width,img.height,0,0, menuDiv.offsetWidth, menuDiv.offsetHeight)
+    // });
   });
 
   return (
